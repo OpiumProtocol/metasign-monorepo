30968e45bcd3074f2d0699229294ae4ae2deb594
Ali Nuraldin <alirunwwc@gmail.com>
2020-02-28 17:20:49 +0100

    First try

--- packages/web3-provider/src/index.ts
+++ packages/web3-provider/src/index.ts
@@ -2,17 +2,43 @@ import ProviderEngine from 'web3-provider-engine'
 // @ts-ignore
 import HookedWalletSubprovider from 'web3-provider-engine/subproviders/hooked-wallet'
 
-class MetaSignProvider extends ProviderEngine {
-  public isMetaSign = true
+interface ISignatureResponse {
+  id: string
+  signature: string
+  address: string
+}
 
-  constructor() {
+interface IClient {
+  connect: () => Promise<void>
+  generateId: () => Promise<string>
+  registerSignatureReceiver: (callback: (response: ISignatureResponse) => Promise<void>) => void
+}
+
+type ProviderOptions = {
+  client: IClient
+}
+
+class MetaSignProvider extends ProviderEngine {
+  public isMetaSign: boolean = true
+
+  private _client: IClient
+  private _pendingSignatures: { [id: string]: (signature: string) => void } = {}
+  
+  private _accounts: string[] = ['0x0000000000000000000000000000000000000000']
+
+  constructor(options: ProviderOptions) {
     super()
+
+    if (!options.client) {
+      throw new Error('MetaSign client is nor provided')
+    }
+
+    this._client = options.client
+
     this.addProvider(
       new HookedWalletSubprovider({
         getAccounts: async (cb: any) => {
-          cb(null, [
-            '0x0000000000000000000000000000000000000000'
-          ]);
+          cb(null, this._accounts);
         },
         processTypedMessage: async (msgParams: { data: string }, cb: any) => {
           try {
@@ -27,15 +53,27 @@ class MetaSignProvider extends ProviderEngine {
   }
 
   public async enable() {
-    // TODO: client.connect
-    console.error('DEBUG: enable() ')
-    // throw new Error('Not implemented')
+    this._client.registerSignatureReceiver(this._signatureReceiver)
+    return this._client.connect()
   }
 
   private async _processTypedMessage(data: string) {
-    // TODO: client.create
-    console.error('DEBUG: _processTypedMessage(data)', data)
-    return '0x12345678'
+    const id = await this._client.generateId()
+    
+    const pending: Promise<string> = new Promise((resolve: (signature: string) => void) => {
+      this._pendingSignatures[id] = resolve
+    })
+
+    // TODO: showQR(data)
+    return pending
+  }
+
+  private async _signatureReceiver(response: ISignatureResponse) {
+    const resolver = this._pendingSignatures[response.id]
+    resolver(response.signature)
+
+    // Set last signer as the only account on provider
+    this._accounts = [response.address]
   }
 }
 
